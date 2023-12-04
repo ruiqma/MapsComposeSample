a8d53ccf0741a682ac683af02a0ae7cf73622fc2
safie-s-kawahara <s-kawahara@safie.jp>
2023-12-04 16:10:15 +0900

    MakerComposable実装

--- app/src/main/java/com/example/mapscomposesample/MainActivity.kt
+++ app/src/main/java/com/example/mapscomposesample/MainActivity.kt
@@ -3,13 +3,33 @@ package com.example.mapscomposesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerComposable
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
@@ -34,10 +54,46 @@ fun MapContent() {
         cameraPositionState = cameraPositionState,
     ) {
         Prefecture.values().forEach {
-            Marker(
+            MarkerComposable(
                 state = MarkerState(LatLng(it.latitude, it.longitude)),
-                title = it.name,
-            )
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    CallOut(it.name)
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Image(
+                        modifier = Modifier.size(45.dp, 30.dp),
+                        painter = painterResource(it.flag),
+                        contentDescription = null,
+                    )
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun CallOut(name: String) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy((-6).dp),
+    ) {
+        Text(
+            text = name,
+            fontSize = 12.sp,
+            modifier = Modifier
+                .drawBehind {
+                    drawRoundRect(color = Color.White, cornerRadius = CornerRadius(12f))
+                }
+                .padding(12.dp, 6.dp)
+        )
+        Box(
+            modifier = Modifier
+                .size(12.dp)
+                .rotate(45f)
+                .clip(RectangleShape)
+                .background(Color.White)
+        )
     }
 }
--- app/src/main/java/com/example/mapscomposesample/Prefecture.kt
+++ app/src/main/java/com/example/mapscomposesample/Prefecture.kt
@@ -1,11 +1,17 @@
 package com.example.mapscomposesample
 
-enum class Prefecture(val latitude: Double, val longitude: Double) {
-    Tokyo(35.689501, 139.691722),
-    Kanagawa(35.447734, 139.642537),
-    Saitama(35.857033, 139.649012),
-    Chiba(35.604560, 140.123154),
-    Ibaraki(36.341737, 140.446824),
-    Tochigi(36.565912, 139.883592),
-    Gunma(36.390688, 139.060453),
+import androidx.annotation.DrawableRes
+
+enum class Prefecture(
+    val latitude: Double,
+    val longitude: Double,
+    @DrawableRes val flag: Int
+) {
+    Tokyo(35.689501, 139.691722, R.drawable.flag_of_tokyo_metropolis),
+    Kanagawa(35.447734, 139.642537, R.drawable.flag_of_kanagawa_prefecture),
+    Saitama(35.857033, 139.649012, R.drawable.flag_of_saitama_prefecture),
+    Chiba(35.604560, 140.123154, R.drawable.flag_of_chiba_prefecture),
+    Ibaraki(36.341737, 140.446824, R.drawable.flag_of_ibaraki_prefecture),
+    Tochigi(36.565912, 139.883592, R.drawable.flag_of_tochigi_prefecture),
+    Gunma(36.390688, 139.060453, R.drawable.flag_of_gunma_prefecture),
 }
